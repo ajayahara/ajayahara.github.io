0ee2488311cc6ae6186e9aae2e1fbc4960f208e0
ajayahara <abak00350@gmail.com>
2023-02-25 09:41:13 +0530

    PortFolio Update

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,47 +3,43 @@ import "../CSS/Contact.css"
 export const Contact = () => {
   return (
     <div className='contact' id='contact'>
-        <h1 className='contact-heading'>Contact Me!</h1>
-        <div className='contact-container'>
-          <div className='my-contact'>
-              <div className='mycontact-div1'>
-                My Contact
-              </div>
-              <div className='mycontact-div2'>
-                <div>
-                  <img src="/Images/mail.png" alt="" />
-                  <span>abak00350@gmail.com</span>
-                </div>
-                <div>
-                  <img src="/Images/call.png" alt="" />
-                  <span>+91 6370014498</span>
-                </div>
-                <div>
-                  <img src="/Images/location.png" alt="" />
-                  <span>Cuttack, Odisha</span>
-                </div>
-                <div>
-                  <img src="/Images/github.png" alt="" />
-                  <a href="https://github.com/ajayahara" target='_blank' rel="noreferrer">
-                  <span>ajayahara</span>
-                  </a>
-                </div>
-                <div>
-                  <img src="/Images/linkedin.png" alt="" />
-                  <a href="https://linkedin.com/in/https://www.linkedin.com/in/ajaya-kumar-behera-a56549237/" target='_blank' rel="noreferrer" >
-                  <span>Ajaya Kumar</span>
-                  </a>
-                </div>
-              </div>
+      <h1 className='contact-heading'>Contact Me!</h1>
+      <div className='contact-container'>
+        <div className='my-contact'>
+          {/* <div className='mycontact-div1'>
+            My Contact
+          </div> */}
+          <div className='mycontact-div2'>
+            <a href="#">
+              <img src="/Images/mail.png" alt="" />
+              <span>abak00350@gmail.com</span>
+            </a>
+            <a href="#">
+              <img src="/Images/call.png" alt="" />
+              <span>+91 6370014498</span>
+            </a>
+            <a href="#">
+              <img src="/Images/location.png" alt="" />
+              <span>Cuttack, Odisha</span>
+            </a>
+            <a href="https://github.com/ajayahara" target='_blank' rel="noreferrer">
+              <img src="/Images/github.png" alt="" />
+              <span>ajayahara</span>
+            </a>
+            <a href="https://linkedin.com/in/https://www.linkedin.com/in/ajaya-kumar-behera-a56549237/" target='_blank' rel="noreferrer" >
+              <img src="/Images/linkedin.png" alt="" />
+              <span>Ajaya Kumar</span>
+            </a>
           </div>
-          <form className='contact-form'>
-                <input type="text" required placeholder='Name Here'/>
-                <input type="email" required placeholder='Email Here'/>
-                <input type="mobile" placeholder='Phone Here' required/>
-                <textarea placeholder='Message Here'></textarea>
-                <input type="submit" value={"Send"} />
-          </form>
         </div>
+        <form className='contact-form'>
+          <input type="text" required placeholder='Name Here' />
+          <input type="email" required placeholder='Email Here' />
+          <input type="mobile" placeholder='Phone Here' required />
+          <textarea placeholder='Message Here'></textarea>
+          <input type="submit" value={"Send"} />
+        </form>
+      </div>
     </div>
   )
 }
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import "../CSS/Project.css"
 export const Project = () => {
   const data = [
-    { name: "Abof", url1: "https://teal-biscuit-947d6f.netlify.app/", url2: "https://github.com/ajayahara/homely-yarn-5441", des1: 'Abof.com is the first D2C digital-first brand from house of Aditya Birla Fashion & Retail.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: 'React, Express, Chakra, Mongoose', resp: 'Product, Login, Register',path:'/Images/abof-logo.webp' }
+    { name: "Abof.Com", url1: "https://teal-biscuit-947d6f.netlify.app/", url2: "https://github.com/ajayahara/homely-yarn-5441", des1: 'Abof.com is the first D2C digital-first brand from house of Aditya Birla Fashion & Retail.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech:[], resp: 'Product page & its functionalities, Login page & Register Page', path: '/Images/abof.png' }
     ,
-    { name: "Jiomart", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: 'React, Chakra, Redux', resp: 'Cart & Payment Page',path:'/Images/jiomart-logo.png' }
+    { name: "Jiomart.Com", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: [], resp: 'Cart & Payment Page', path: '/Images/jiomart.png' }
     ,
-    { name: "SugarCosmetics", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: 'React,Chakra,Redux', resp: 'Cart & Payment Page' ,path:'/Images/SUGAR-Cosmetics-logo.png'}
+    { name: "SugarCosmetics", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: [], resp: 'Cart & Payment Page', path: '/Images/sugar.png' }
     ,
-    { name: "Nykaa", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: 'React,Chakra,Redux', resp: 'Cart & Payment Page', path:'/Images/nykaa-logo.png'}
+    { name: "Nykaa", url1: "https://melodic-wisp-f3c5fb.netlify.app/", url2: "https://github.com/ajayahara/JIOMART.COM_RCT_201", des1: 'JioMart is an Indian e-commerce company . It makes your grocery shopping even simpler. No more hassles of sweating it out in crowded markets, grocery shops & supermarkets.', des2: 'A collaborative project cloned within five days. By cloning this website we are trying to implement some functionality.', tech: [], resp: 'Cart & Payment Page', path: '/Images/nykaa.png' }
   ]
   return (
     <div className='project' id='project'>
@@ -18,26 +18,27 @@ export const Project = () => {
           return <div className='project-item' key={i}>
             <div className='item-left'>
               <img src={el.path} alt="" />
-              <div className='item-name'>
-               {el.name}
-              </div>
-              <div className='button'>
-                <button><a href={el.url2} target='_blank'  rel="noreferrer" >Github</a></button>
-                <button><a href={el.url1}  rel="noreferrer" target='_blank'>Live</a></button>
-              </div>
             </div>
             <div className='item-right'>
-              <div>
-                -Responsibility:{el.resp}
-              </div>
-              <div>
-                -Tech Stacks:{el.tech}
+              <div className='item-name'>
+                {el.name}
               </div>
               <div>
                 -{el.des1}
               </div>
               <div>
-               -{el.des2}
+                -{el.des2}
+              </div>
+
+              <div>
+                -Responsibility:{el.resp}
+              </div>
+              <div className='techstack' tooltip="React" >
+                -Tech Stacks: <img src="/Images/react.png" alt="" /><img src="/Images/mongodb.png" alt="" /><img src="/Images/chakra.jfif" alt="" /><img src="/Images/redux.png" alt="" />
+              </div>
+              <div className='button'>
+                <button><a href={el.url2} target='_blank' rel="noreferrer" >Github</a></button>
+                <button><a href={el.url1} rel="noreferrer" target='_blank'>Live</a></button>
               </div>
             </div>
           </div>
